8495ce69cb8b6293af2e923ce977ae98e4973b69
mrdulin <novaline.dulin@gmail.com>
2020-05-19 12:42:28 +0800

    fix import issue

--- src/app.js
+++ src/app.js
@@ -1,42 +1,36 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
-import Yelp from '../../util/Yelp'
-
+import BusinessList from './component/BusinessList';
+import SearchBar from './component/SearchBar';
+import Yelp from './util/Yelp';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      businesses: []
+      businesses: [],
     };
     this.searchYelp = this.searchYelp.bind(this);
   }
 
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(
-      businesses => {
-        this.setState({
-          businesses: businesses
-        })
-      }
-    )
+    Yelp.search(term, location, sortBy).then((businesses) => {
+      this.setState({
+        businesses: businesses,
+      });
+    });
   }
 
   render() {
     return (
       <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar searchYelp={this.searchYelp} />
-      <BusinessList businesses={this.state.businesses}/>
-  
-</div>
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
-
 
 export default App;
